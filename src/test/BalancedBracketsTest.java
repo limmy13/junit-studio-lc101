8d1e96d45cc76f40cdb000483492a69401f7ea0f
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-06-21 17:37:42 -0500

    started writing first few tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,38 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void stringWithBalancedBrackets() {
+    String spec = "Tests a string that contains balanced brackets";
+    String expected = "[LaunchCode]";
+    assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
+    }
+
+    @Test
+    public void stringWithBalanceBracketsAndNonBracketChars() {
+        String spec = "Tests a string that contains balanced brackets with non-bracket characters";
+        String expected = "Launch[Code]";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
+    }
+
+    @Test
+    public void stringThatStartsWithBrackets(){
+        String spec = "Tests a string that starts and ends with brackets.";
+        String expected = "[]LaunchCode";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
+    }
+
+
 
 
 }
