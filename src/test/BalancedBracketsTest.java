28077b78e931174078aa2e1180bc768d4b2b140c
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-06-21 17:41:08 -0500

    tests 5 and 6

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -10,7 +10,7 @@ public class BalancedBracketsTest {
     //TODO: add tests here
 
     @Test
-    public void onlyBracketsReturnsTrue() {
+    public void onlyBracketsReturnsTrue() { //1
         assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
     @Test
@@ -18,28 +18,39 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
-    @Test
+    @Test //2
     public void stringWithBalancedBrackets() {
     String spec = "Tests a string that contains balanced brackets";
     String expected = "[LaunchCode]";
     assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
     }
 
-    @Test
+    @Test //3
     public void stringWithBalanceBracketsAndNonBracketChars() {
         String spec = "Tests a string that contains balanced brackets with non-bracket characters";
         String expected = "Launch[Code]";
         assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
     }
 
-    @Test
+    @Test //4
     public void stringThatStartsWithBrackets(){
         String spec = "Tests a string that starts and ends with brackets.";
         String expected = "[]LaunchCode";
         assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
     }
 
+    @Test //5
+    public void emptyStringTest() {
+        String spec = "Tests empty string";
+        String expected = "";
+        assertTrue(BalancedBrackets.hasBalancedBrackets(expected));
+    }
 
-
+    @Test //6
+    public void stringWithUnbalancedBrackets() {
+        String spec = "Tests a string that contains unbalanced brackets";
+        String expected = "[LaunchCode";
+        assertFalse(BalancedBrackets.hasBalancedBrackets(expected));
+    }
 
 }
